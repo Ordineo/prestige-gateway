f21b6cac28a04c453fcd4c47d3ad38829bc55d5f
Tyranwyn <sammi.fux@hotmail.com>
2017-04-27 16:48:52 +0200

    PRES-94: simple jwt generation

---
+++ src/main/java/be/ordina/prestige/model/AccountCredentials.java
@@ -0,0 +1,14 @@
+package be.ordina.prestige.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by SaFu on 27/04/2017.
+ */
+@Getter
+@Setter
+public class AccountCredentials {
+    private String username;
+    private String password;
+}
